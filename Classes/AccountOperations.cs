74b597c087bf5ba41638a8beb7d797caaefb8cc1
Gabby <gabbzf@gmail.com>
2024-11-13 13:35:23 +0100

    cleaned up code and added some additional styling.

--- Classes/AccountOperations.cs
+++ Classes/AccountOperations.cs
@@ -9,54 +9,68 @@ namespace BankAccountApp.Classes
 
         public static void CheckBalance(HandleBankDB handleBankDB)
         {
-            Console.WriteLine("\nEnter the account for wich you want to check balance:");
-            var selectedAccountCheckBalance = InputValidation.ValidateAccountNumber(handleBankDB.AllAccountsFromDB);
+            var accountChoices = handleBankDB.AllAccountsFromDB.Select(account => $"{account.AccountType} - {account.AccountNumber}").ToList();
 
-            AnsiConsole.Progress()
-            .Start(ctx =>
+            var selectedAccountChoice = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("Choose an account to check balance:")
+                    .PageSize(10) 
+                    .AddChoices(accountChoices)
+                    .HighlightStyle(new Style(Color.DarkTurquoise)) 
+            );
+
+            var selectedAccount = handleBankDB.AllAccountsFromDB.FirstOrDefault(account => $"{account.AccountType} - {account.AccountNumber}" == selectedAccountChoice);
+
+            if (selectedAccount != null)
             {
-                var task = ctx.AddTask("[green]Loading account...[/]");
-
-                while (!task.IsFinished)
-                {
-                    task.Increment(10);
-                    Thread.Sleep(100);
-                }
-
-                task.Description = "[green]Load complete![/]";
-            });
-
-            selectedAccountCheckBalance.CheckBalance();
+                selectedAccount.CheckBalance(); 
+            }
+            else
+            {
+                AnsiConsole.MarkupLine("[red]Account not found.[/]");
+            }
         }
 
         public static void DepositMoney(string dataJSONfilePath, HandleBankDB handleBankDB)
         {
-            Console.WriteLine("\nEnter the account number of the account to deposit money to:");
-            var selectedAccountDepositTo = InputValidation.ValidateAccountNumber(handleBankDB.AllAccountsFromDB);
+            var selectedAccountDepositTo = ChooseAccount(handleBankDB, "Choose an account to deposit money into:");
 
-            Console.WriteLine($"You selected account number {selectedAccountDepositTo.AccountNumber}. Proceeding with deposit.");
-
-            double depositAmount = InputValidation.ValidateNumericInput();
-
-            AnsiConsole.Progress()
-            .Start(ctx =>
+            if (selectedAccountDepositTo != null)
             {
-                var task = ctx.AddTask("[green]Transaction in progress...[/]");
+                Console.WriteLine($"You selected account number {selectedAccountDepositTo.AccountNumber}. Proceeding with deposit.");
 
-                while (!task.IsFinished)
+                double depositAmount = AnsiConsole.Ask<double>("[lightseagreen]Enter the amount to deposit:[/]");
+
+                if (depositAmount <= 0)
                 {
-                    task.Increment(10); 
-                    Thread.Sleep(100); 
+                    AnsiConsole.MarkupLine("[red]Deposit amount must be greater than 0.[/]");
+                    return;
                 }
 
-                task.Description = "[green]Transaction completed![/]";
-            });
+                AnsiConsole.Progress()
+                .Start(ctx =>
+                {
+                    var task = ctx.AddTask("[lightseagreen]Transaction in progress...[/]");
 
-            selectedAccountDepositTo.Deposit(depositAmount);
+                    while (!task.IsFinished)
+                    {
+                        task.Increment(10);
+                        Thread.Sleep(100);
+                    }
 
-            File.WriteAllText(dataJSONfilePath, JsonSerializer.Serialize(handleBankDB, new JsonSerializerOptions { WriteIndented = true }));
+                    task.Description = "[lightseagreen]Transaction completed![/]";
+                });
 
-            Console.WriteLine($"You have successfully deposited {depositAmount} into account {selectedAccountDepositTo.AccountType}. New balance: {selectedAccountDepositTo.Balance}.");
+                selectedAccountDepositTo.Deposit(depositAmount);
+
+                File.WriteAllText(dataJSONfilePath, JsonSerializer.Serialize(handleBankDB, new JsonSerializerOptions { WriteIndented = true }));
+
+                Console.WriteLine($"You have successfully deposited {depositAmount:C} into account {selectedAccountDepositTo.AccountType}. New balance: {selectedAccountDepositTo.Balance:C}.");
+            }
+            else
+            {
+                AnsiConsole.MarkupLine("[red]Account not found.[/]");
+            }
         }
 
         public static void ListAllAccounts(HandleBankDB handleBankDB)
@@ -64,7 +78,7 @@ namespace BankAccountApp.Classes
             AnsiConsole.Progress()
             .Start(ctx =>
             {
-                var task = ctx.AddTask("[green]Loading accounts...[/]");
+                var task = ctx.AddTask("[lightseagreen]Loading accounts...[/]");
 
                 while (!task.IsFinished)
                 {
@@ -72,60 +86,84 @@ namespace BankAccountApp.Classes
                     Thread.Sleep(100); 
                 }
 
-                task.Description = "[green]Load complete![/]";
+                task.Description = "[lightseagreen]Load complete![/]";
             });
-            Console.WriteLine("\nThese are your accounts:");
+            var table = new Table();
+
+            table.AddColumn(new TableColumn("[bold lightseagreen]Account Type[/]").LeftAligned());
+            table.AddColumn(new TableColumn("[bold lightseagreen]Account Number[/]").LeftAligned());
+            table.AddColumn(new TableColumn("[bold lightseagreen]Balance[/]").LeftAligned());
+
             foreach (var account in handleBankDB.AllAccountsFromDB)
             {
-                Console.WriteLine($"Account Type: {account.AccountType}");
-                Console.WriteLine($"Account Number: {account.AccountNumber}");
-                Console.WriteLine($"Balance: {account.Balance}");
+                table.AddRow(account.AccountType, account.AccountNumber.ToString(), $"{account.Balance:C}");
             }
+
+            table.Border = TableBorder.Rounded;
+            table.LeftAligned();
+            table.BorderColor(Color.LightSeaGreen);
+            AnsiConsole.Write(table);
         }
 
         public static void TransferHistory(HandleBankDB handleBankDB)
         {
-            Console.WriteLine("\nEnter the account for wich you want to check transfer history:");
-            var selectedAccountCheckTransferHistory = InputValidation.ValidateAccountNumber(handleBankDB.AllAccountsFromDB);
-            AnsiConsole.Progress()
-            .Start(ctx =>
+            var selectedAccountCheckTransferHistory = ChooseAccount(handleBankDB, "Choose an account to check transfer history:");
+
+            if (selectedAccountCheckTransferHistory != null)
             {
-                var task = ctx.AddTask("[green]Loading account history...[/]");
-
-                while (!task.IsFinished)
+                AnsiConsole.Progress()
+                .Start(ctx =>
                 {
-                    task.Increment(10);
-                    Thread.Sleep(100);
-                }
+                    var task = ctx.AddTask("[lightseagreen]Loading account history...[/]");
 
-                task.Description = "[green]Load complete![/]";
-            });
-            selectedAccountCheckTransferHistory.ShowTransactionHistory();
+                    while (!task.IsFinished)
+                    {
+                        task.Increment(10);
+                        Thread.Sleep(100);
+                    }
+
+                    task.Description = "[lightseagreen]Load complete![/]";
+                });
+
+                selectedAccountCheckTransferHistory.ShowTransactionHistory();
+            }
+            else
+            {
+                AnsiConsole.MarkupLine("[red]Account not found.[/]");
+            }
         }
 
         public static void TransferMoney(string dataJSONfilePath, HandleBankDB handleBankDB)
         {
-            Console.WriteLine("\nEnter the account number to transfer money from:");
-            var selectedAccountTransferFrom = InputValidation.ValidateAccountNumber(handleBankDB.AllAccountsFromDB);
+            var selectedAccountTransferFrom = ChooseAccount(handleBankDB, "Choose the account to transfer money from:");
+            if (selectedAccountTransferFrom == null)
+            {
+                AnsiConsole.MarkupLine("[red]No account selected.[/]");
+                return;
+            }
 
-            Console.WriteLine($"Enter the account number to transfer money to:");
-            var selectedAccountTransferTo = InputValidation.ValidateAccountNumber(handleBankDB.AllAccountsFromDB);
+            var selectedAccountTransferTo = ChooseAccount(handleBankDB, "Choose the account to transfer money to:");
+            if (selectedAccountTransferTo == null)
+            {
+                AnsiConsole.MarkupLine("[red]No account selected.[/]");
+                return;
+            }
 
-            double transferAmount = InputValidation.ValidateAmountInput(selectedAccountTransferFrom.Balance);
+            double transferAmount = ValidateAmountInput(selectedAccountTransferFrom.Balance);
 
             AnsiConsole.Progress()
-            .Start(ctx =>
-            {
-                var task = ctx.AddTask("[green]Transaction in progress...[/]");
-
-                while (!task.IsFinished)
+                .Start(ctx =>
                 {
-                    task.Increment(10); 
-                    Thread.Sleep(100); 
-                }
+                    var task = ctx.AddTask("[lightseagreen]Transaction in progress...[/]");
 
-                task.Description = "[green]Transaction completed![/]";
-            });
+                    while (!task.IsFinished)
+                    {
+                        task.Increment(10);
+                        Thread.Sleep(100);
+                    }
+
+                    task.Description = "[lightseagreen]Transaction completed![/]";
+                });
 
             selectedAccountTransferFrom.Transfer(transferAmount, selectedAccountTransferFrom, selectedAccountTransferTo);
 
@@ -134,32 +172,82 @@ namespace BankAccountApp.Classes
 
         public static void WithDrawMoney(string dataJSONfilePath, HandleBankDB handleBankDB)
         {
-            Console.WriteLine("\nEnter the account number of the account to withdraw money from:");
-            var selectedAccountWithdrawFrom = InputValidation.ValidateAccountNumber(handleBankDB.AllAccountsFromDB);
+            var selectedAccountWithdrawFrom = ChooseAccount(handleBankDB, "Choose an account to withdraw money from:");
 
-            Console.WriteLine($"You selected account number {selectedAccountWithdrawFrom.AccountNumber}. Proceeding with withdrawal.");
-
-            double withdrawalAmount = InputValidation.ValidateAmountInput(selectedAccountWithdrawFrom.Balance);
-
-            AnsiConsole.Progress()
-            .Start(ctx =>
+            if (selectedAccountWithdrawFrom != null)
             {
-                var task = ctx.AddTask("[green]Transaction in progress...[/]");
+                Console.WriteLine($"You selected account number {selectedAccountWithdrawFrom.AccountNumber}. Proceeding with withdrawal.");
 
-                while (!task.IsFinished)
+                double withdrawalAmount = ValidateAmountInput(selectedAccountWithdrawFrom.Balance);
+
+                AnsiConsole.Progress()
+                .Start(ctx =>
                 {
-                    task.Increment(10); 
-                    Thread.Sleep(100); 
+                    var task = ctx.AddTask("[lightseagreen]Transaction in progress...[/]");
+
+                    while (!task.IsFinished)
+                    {
+                        task.Increment(10);
+                        Thread.Sleep(100);
+                    }
+
+                    task.Description = "[lightseagreen]Transaction completed![/]";
+                });
+
+                selectedAccountWithdrawFrom.Withdraw(withdrawalAmount);
+
+                File.WriteAllText(dataJSONfilePath, JsonSerializer.Serialize(handleBankDB, new JsonSerializerOptions { WriteIndented = true }));
+
+                Console.WriteLine($"You have successfully withdrawn {withdrawalAmount:C} from account {selectedAccountWithdrawFrom.AccountType}. New balance: {selectedAccountWithdrawFrom.Balance:C}.");
+            }
+            else
+            {
+                AnsiConsole.MarkupLine("[red]Account not found.[/]");
+            }
+        }
+        public static BankAccount? ChooseAccount(HandleBankDB handleBankDB, string promptMessage)
+        {
+            var accountChoices = handleBankDB.AllAccountsFromDB.Select(account => $"{account.AccountType} - {account.AccountNumber}").ToList();
+
+            var selectedAccountChoice = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title(promptMessage)
+                    .PageSize(10)
+                    .AddChoices(accountChoices)
+                    .HighlightStyle(new Style(Color.DarkTurquoise))
+            );
+
+            var selectedAccount = handleBankDB.AllAccountsFromDB.FirstOrDefault(account => $"{account.AccountType} - {account.AccountNumber}" == selectedAccountChoice);
+
+            return selectedAccount; 
+        }
+        public static double ValidateAmountInput(double balance)
+        {
+            while (true)
+            {
+                Console.Write("Enter amount: ");
+                string amountInput = Console.ReadLine()!;
+
+                if (double.TryParse(amountInput, out double amount))
+                {
+                    if (amount > balance)
+                    {
+                        AnsiConsole.MarkupLine("[red]You don't have that much money in the account.[/]");
+                    }
+                    else if (amount <= 0)
+                    {
+                        AnsiConsole.MarkupLine("[red]The amount must be greater than zero.[/]");
+                    }
+                    else
+                    {
+                        return amount;
+                    }
                 }
-
-                task.Description = "[green]Transaction completed![/]";
-            });
-
-            selectedAccountWithdrawFrom.Withdraw(withdrawalAmount);
-
-            File.WriteAllText(dataJSONfilePath, JsonSerializer.Serialize(handleBankDB, new JsonSerializerOptions { WriteIndented = true }));
-
-            Console.WriteLine($"You have successfully withdrawn {withdrawalAmount} from account {selectedAccountWithdrawFrom.AccountType}. New balance: {selectedAccountWithdrawFrom.Balance}.");
+                else
+                {
+                    AnsiConsole.MarkupLine("[red]Invalid input. Please enter a valid number.[/]");
+                }
+            }
         }
     }
 }
